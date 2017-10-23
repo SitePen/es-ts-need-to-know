64fcf4fee9fee8f4a7d3a601f4993c00bd2cfaeb
Nick Nisi <nick@nisi.org>
2017-10-22 21:31:05 -0500

    starting point

---
+++ src/ArticlesComponent.js
@@ -0,0 +1,58 @@
+define([
+	'exports',
+	'./util'
+], function (exports, util) {
+	function ArticlesComponent(domNode, articles) {
+		if (!domNode) {
+			this.domNode = document.createElement('div');
+		}
+		else if (typeof domNode === 'string') {
+			this.domNode = document.querySelector(domNode);
+		}
+		else {
+			this.domNode = domNode;
+		}
+		this.articles = articles || [];
+	}
+
+	ArticlesComponent.prototype.render = function() {
+		this.domNode.innerHTML = '';
+
+		this.articles.forEach(function (article) {
+			var articleDiv = this.renderArticle(article);
+			this.domNode.appendChild(articleDiv);
+		}, this);
+
+		return this.domNode;
+	};
+
+	ArticlesComponent.prototype.setArticles = function (articles) {
+		this.articles = articles;
+		this.render();
+	};
+
+	ArticlesComponent.prototype.renderArticle = function (article) {
+		var div = document.createElement('div')
+		div.className = 'article';
+		div.innerHTML = [
+			'<div class="image">',
+				'<img src="' + article.urlToImage + '" />',
+			'</div>',
+			'<div class="content">',
+				'<div class="header">',
+					'<a href="' + article.url + '">' + article.title + '</a>',
+				'</div>',
+				'<div class="meta date">',
+					util.toRelativeDate(article.publishedAt),
+				'</div>',
+				'<div class="meta description">',
+				'<p>' + article.description + '</p>',
+				'</div>',
+			'</div>'
+		].join('\n');
+
+		return div;
+	};
+
+	return ArticlesComponent;
+});
