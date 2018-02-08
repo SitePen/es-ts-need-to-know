5e7736af1340bb6bbbbb86e9532399ec5106ccc5
Nick Nisi <nick@nisi.org>
2018-02-07 23:55:48 -0600

    add ArticlesService

--- src/index.js
+++ src/index.js
@@ -1,45 +1,19 @@
 require([
+	'./ArticlesService',
 	'./ArticlesComponent'
-], function (ArticlesComponent) {
+], function (ArticlesService, ArticlesComponent) {
+	// Note: register for your own API key at https://newsapi.org/register
 	var API_KEY = '2b88a66af2114afcb1f736a302d51998';
-
-	var newsSource = {
-		NYT: 'the-new-york-times',
-		WAPO: 'the-washington-post',
-		HN: 'hacker-news',
-		TNW: 'the-next-web',
-		VERGE: 'the-verge',
-		ARS: 'ars-technica'
-	};
-
-	function fetchArticles(source, callback, errorCallback) {
-		var url = 'https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + API_KEY;
-		var xhr = new XMLHttpRequest();
-
-		xhr.onreadystatechange = function () {
-			var response;
-			if (xhr.readyState === 4) {
-				if (xhr.status ===  200) {
-					response = JSON.parse(xhr.responseText);
-					callback(response.articles);
-				}
-				else {
-					errorCallback && errorCallback(xhr.statusText);
-				}
-			}
-		};
-
-		xhr.open('GET', url);
-		xhr.send();
-	}
+	var newsSource = ArticlesService.newsSource;
+	var service = new ArticlesService(API_KEY);
 
 	function loadArticles() {
 		var articlesDiv = document.getElementById('articles');
 		var component = new ArticlesComponent(articlesDiv);
-		fetchArticles(newsSource.VERGE, function (articles) {
+		service.getArticles(newsSource.VERGE, function (articles) {
 			component.setArticles(articles);
 		}, function (error) {
-			console.log('[ERROR]', error)
+			console.error('[ERROR]', error)
 		});
 	}
 
